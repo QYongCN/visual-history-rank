6a5f37691bd2c64a238638b80aafba48cd1ff1d8
QYongCN <40787180+QYongCN@users.noreply.github.com>
2020-04-25 11:31:41 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    SVGWidth:       900,
+    SVGHeight:      450,
+    TimerFontSize:  "50px",
+    MaxNumber:      25,
+    Padding:{
+        left:       230,
+        right:      150,
+        top:        50,
+        bottom:     0
+    },
+    ColorClass:     ["A", "B", "C", "D", "E", "F"],
+    IntervalTime:   0.33333,
+    XTicks:         10
+}
